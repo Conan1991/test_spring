02db7779e1f9aaec53406e670bd87d823320a80f
v.doronin <v.doronin@rivc-pulkovo.ru>
2023-07-28 10:05:30 +0300

    commit

--- src/main/java/com/example/test_spring/test/Permit.java
+++ src/main/java/com/example/test_spring/test/Permit.java
@@ -1,7 +1,24 @@
 package com.example.test_spring.test;
 
+import java.time.Instant;
+
 public class Permit {
     private Integer reserveId;
+
+    public Instant getBeginPlanTime() {
+        return beginPlanTime;
+    }
+
+    public void setBeginPlanTime(Instant beginPlanTime) {
+        this.beginPlanTime = beginPlanTime;
+    }
+
+    private Instant beginPlanTime;
+
+    public Permit(Integer reserveId, Instant beginPlanTime) {
+        this.reserveId = reserveId;
+        this.beginPlanTime = beginPlanTime;
+    }
 
     public Permit(Integer reserveId) {
         this.reserveId = reserveId;
--- src/test/java/com/example/test_spring/TestSpringApplicationTests.java
+++ src/test/java/com/example/test_spring/TestSpringApplicationTests.java
@@ -8,10 +8,18 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.math.BigDecimal;
+import java.time.Duration;
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+import java.time.temporal.ChronoUnit;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Optional;
-import java.util.stream.Stream;
+
 
 @SpringBootTest
 @Slf4j
@@ -90,12 +98,39 @@ class TestSpringApplicationTests {
 
     @Test
     void contextLoads5() {
-        //Stream<Permit> stream = Stream.of(new Permit(0), new Permit(0), new Permit(1), new Permit(2));
-        Stream<Permit> stream = Stream.of(new Permit(2));
+        Instant now = Instant.now();
+        List<Permit> list = List.of(new Permit(0, now),
+                new Permit(3, now.plus(5, ChronoUnit.HOURS)),
+                new Permit(1, now.minus(5, ChronoUnit.HOURS)),
+                new Permit(2, now.plus(6, ChronoUnit.HOURS)));
+        //Stream<Permit> stream = Stream.of(new Permit(2, Instant.now()));
 
-        Permit lastElement = stream.reduce((first, second) -> second)
+
+        Permit lastElement = list
+                .stream()
+                .max(Comparator.comparing(Permit::getBeginPlanTime))
                 .orElse(null);
 
-        log.info("test last: {}", lastElement.getReserveId());
+        List<Permit> sorted = list.stream().sorted(Comparator.comparing(Permit::getBeginPlanTime)).toList();
+
+        Permit permit = sorted.stream().findFirst().orElse(null);
+
+        sorted.forEach(p -> log.info("test all: {}, time: {}", p.getReserveId(), p.getBeginPlanTime()));
+        log.info("test last: {}, time: {}", lastElement.getReserveId(), lastElement.getBeginPlanTime());
+        log.info("test first: {}, time: {}", permit.getReserveId(), permit.getBeginPlanTime());
+    }
+
+    @Test
+    void contextLoads6() {
+        Instant now = LocalDateTime.of(2023, 7, 26, 13, 10 , 49).toInstant(ZoneOffset.UTC);
+        Instant plus = LocalDateTime.of(2023, 7, 26, 18, 11 , 50).toInstant(ZoneOffset.UTC);
+        BigDecimal bigDecimal = BigDecimal.valueOf(0.3465d);
+        log.info("test now: {}", now);
+        log.info("test plus: {}", plus);
+        int intValue = BigDecimal.valueOf(Duration.between(now, plus).toMinutes()).multiply(bigDecimal).intValue();
+        double d = Duration.between(now, plus).toMinutes() * bigDecimal.doubleValue();
+        log.info("test double: {}", d);
+        //log.info("test floor: {}", Math.floor(d));
+        log.info("test int: {}", intValue);
     }
 }
